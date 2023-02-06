f9b8b085256f9178978421fca2311e4a2023993e
waldsteiner <waldsteiner@gmail.com>
2023-02-06 23:54:11 +0900

    setting commit

---
+++ server/Physical-Ativity-Statistics-Service/src/main/java/com/ptsports/PhysicalAtivityStatisticsService/PhysicalAtivityStatisticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ptsports.PhysicalAtivityStatisticsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicalAtivityStatisticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicalAtivityStatisticsServiceApplication.class, args);
+	}
+
+}
---
+++ server/Physical-Ativity-Statistics-Service/src/test/java/com/ptsports/PhysicalAtivityStatisticsService/PhysicalAtivityStatisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptsports.PhysicalAtivityStatisticsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicalAtivityStatisticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
