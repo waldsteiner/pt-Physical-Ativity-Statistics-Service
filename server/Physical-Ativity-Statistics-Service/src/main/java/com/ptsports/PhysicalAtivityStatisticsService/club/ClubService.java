bea2febbc948854e2e4a28c008a24e164f20e60f
waldsteiner <waldsteiner@gmail.com>
2023-02-28 23:55:15 +0900

    #yj_commit_0112   - 기본 패키지 세팅   - 구조별 기본 클래스 추가 중

--- server/Physical-Ativity-Statistics-Service/src/main/java/com/ptsports/PhysicalAtivityStatisticsService/club/ClubService.java
+++ server/Physical-Ativity-Statistics-Service/src/main/java/com/ptsports/PhysicalAtivityStatisticsService/club/ClubService.java
@@ -1,4 +1,12 @@
 package com.ptsports.PhysicalAtivityStatisticsService.club;
 
 public interface ClubService {
+    // 클럽 정보 생성
+    // 클럽 정보 조회
+    // 클럽 정보 모두 조회
+    // 클럽 정보 수정
+    // 클럽 정보 전체 수정
+    // 클럽 정보 삭제
+    // 클럽 정보 검증
+    // 클럽 이름 확인
 }
